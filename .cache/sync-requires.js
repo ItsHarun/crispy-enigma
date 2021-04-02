cd8b7d4657767c53189f4675d25697cadab9de45
ItsHarun <harunrashidvnr8@gmail.com>
2021-04-02 17:39:17 +0530

    Second commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,24 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/layouts/index.js"))
+}
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/dev-404-page.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/haroonrashid/Documents/new/crispy-enigma/src/pages/index.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/haroonrashid/Documents/new/crispy-enigma/src/pages/404.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/layout-index.json"),
+  "index.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/index.json"),
+  "layout-index.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/layout-index.json"),
+  "404.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/404.json"),
+  "layout-index.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/haroonrashid/Documents/new/crispy-enigma/.cache/json/404-html.json")
+}
